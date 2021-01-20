5fd5f5bf70150aa229421e2de76fabdb3ce71513
Charles Thomas <chuckthemole@gmail.com>
2021-01-20 15:46:07 -0800

    Fix working pairs of select options

--- teach/static/teach/js/select_dynamic_options.js
+++ teach/static/teach/js/select_dynamic_options.js
@@ -1,37 +1,94 @@
-// Framework taken from https://www.dyn-web.com/tutorials/forms/select/paired.php
-
-// This js file is meant for use with create_lesson.html
-
 // object literal holding data for option elements
 var Select_List_Data = {
 
-  'subjects': { // name of associated select box
+    'choices': { // name of associated select box
 
-    // names match option values in controlling select box
-    math: {
-      text: ['Scrolling Divs', 'Tooltips', 'Rotate Images', 'Scrollers', 'Banner Rotator'],
-      value: ['scroll', 'tooltips', 'rotate', 'scrollers', 'banner']
-    },
-    science: {
-      text: ['Random Image', 'Form Class', 'Table Class', 'Order Form'],
-      value: ['random', 'form', 'table', 'order']
-    },
-    english: {
-      // example without values
-      text: ['Iframes', 'PHP to JS', 'Object Literals', 'Initializing JS']
-    },
-    history: {
-      // example without values
-      text: ['Iframes', 'PHP to JS', 'Object Literals', 'Initializing JS']
+        // names match option values in controlling select box
+        math: {
+            text: ['Scrolling Divs', 'Tooltips', 'Rotate Images', 'Scrollers', 'Banner Rotator'],
+            value: ['scroll', 'tooltips', 'rotate', 'scrollers', 'banner']
+        },
+        science: {
+            text: ['Random Image', 'Form Class', 'Table Class', 'Order Form'],
+            value: ['random', 'form', 'table', 'order']
+        },
+        english: {
+            // example without values
+            text: ['Iframes', 'PHP to JS', 'Object Literals', 'Initializing JS']
+        },
+        history: {
+            // example without values
+            text: ['Iframes', 'PHP to JS', 'Object Literals', 'Initializing JS']
+        }
+
+    }
+};
+
+// removes all option elements in select box
+// removeGrp (optional) boolean to remove optgroups
+function removeAllOptions(sel, removeGrp) {
+    var len, groups, par;
+    if (removeGrp) {
+        groups = sel.getElementsByTagName('optgroup');
+        len = groups.length;
+        for (var i=len; i; i--) {
+            sel.removeChild( groups[i-1] );
+        }
     }
 
-  }
-};
+    len = sel.options.length;
+    for (var i=len; i; i--) {
+        par = sel.options[i-1].parentNode;
+        par.removeChild( sel.options[i-1] );
+    }
+}
+
+function appendDataToSelect(sel, obj) {
+    var f = document.createDocumentFragment();
+    var labels = [], group, opts;
+
+    function addOptions(obj) {
+        var f = document.createDocumentFragment();
+        var o;
+
+        for (var i=0, len=obj.text.length; i<len; i++) {
+            o = document.createElement('option');
+            o.appendChild( document.createTextNode( obj.text[i] ) );
+
+            if ( obj.value ) {
+                o.value = obj.value[i];
+            }
+
+            f.appendChild(o);
+        }
+        return f;
+    }
+
+    if ( obj.text ) {
+        opts = addOptions(obj);
+        f.appendChild(opts);
+    } else {
+        for ( var prop in obj ) {
+            if ( obj.hasOwnProperty(prop) ) {
+                labels.push(prop);
+            }
+        }
+
+        for (var i=0, len=labels.length; i<len; i++) {
+            group = document.createElement('optgroup');
+            group.label = labels[i];
+            f.appendChild(group);
+            opts = addOptions(obj[ labels[i] ] );
+            group.appendChild(opts);
+        }
+    }
+    sel.appendChild(f);
+}
 
 // anonymous function assigned to onchange event of controlling select box
 document.forms['create_lesson'].elements['subject'].onchange = function(e) {
     // name of associated select box
-    var relName = 'subjects';
+    var relName = 'choices';
 
     // reference to associated select box
     var relList = this.form.elements[ relName ];
@@ -47,87 +104,26 @@ document.forms['create_lesson'].elements['subject'].onchange = function(e) {
     appendDataToSelect(relList, obj);
 };
 
+
 // populate associated select box as page loads
 (function() { // immediate function to avoid globals
 
-  var form = document.forms['create_lesson'];
+    var form = document.forms['create_lesson'];
 
-  // reference to controlling select box
-  var sel = form.elements['subject'];
-  sel.selectedIndex = 0;
+    // reference to controlling select box
+    var sel = form.elements['subject'];
+    sel.selectedIndex = 0;
 
-  // name of associated select box
-  var relName = 'subjects';
-  // reference to associated select box
-  var rel = form.elements[relName];
+    // name of associated select box
+    var relName = 'choices';
+    // reference to associated select box
+    var rel = form.elements[ relName ];
 
-  // get data for associated select box passing its name
-  // and value of selected in controlling select box
-  var data = Select_List_Data[relName][sel.value];
+    // get data for associated select box passing its name
+    // and value of selected in controlling select box
+    var data = Select_List_Data[ relName ][ sel.value ];
 
-  // add options to associated select box
-  appendDataToSelect(rel, data);
+    // add options to associated select box
+    appendDataToSelect(rel, data);
 
 }());
-
-// removes all option elements in select box
-// removeGrp (optional) boolean to remove optgroups
-function removeAllOptions(sel, removeGrp) {
-  var len, groups, par;
-  if (removeGrp) {
-    groups = sel.getElementsByTagName('optgroup');
-    len = groups.length;
-    for (var i = len; i; i--) {
-      sel.removeChild(groups[i - 1]);
-    }
-  }
-
-  len = sel.options.length;
-  for (var i = len; i; i--) {
-    par = sel.options[i - 1].parentNode;
-    par.removeChild(sel.options[i - 1]);
-  }
-}
-
-function appendDataToSelect(sel, obj) {
-  var f = document.createDocumentFragment();
-  var labels = [],
-    group, opts;
-
-  function addOptions(obj) {
-    var f = document.createDocumentFragment();
-    var o;
-
-    for (var i = 0, len = obj.text.length; i < len; i++) {
-      o = document.createElement('option');
-      o.appendChild(document.createTextNode(obj.text[i]));
-
-      if (obj.value) {
-        o.value = obj.value[i];
-      }
-
-      f.appendChild(o);
-    }
-    return f;
-  }
-
-  if (obj.text) {
-    opts = addOptions(obj);
-    f.appendChild(opts);
-  } else {
-    for (var prop in obj) {
-      if (obj.hasOwnProperty(prop)) {
-        labels.push(prop);
-      }
-    }
-
-    for (var i = 0, len = labels.length; i < len; i++) {
-      group = document.createElement('optgroup');
-      group.label = labels[i];
-      f.appendChild(group);
-      opts = addOptions(obj[labels[i]]);
-      group.appendChild(opts);
-    }
-  }
-  sel.appendChild(f);
-}
