05e30b762e3c1325f10e622000127d02589ce94e
chuckthemole <chuckthemole@gmail.com>
2021-01-23 08:51:38 -0800

    Change name on form, scroll index

--- teach/forms.py
+++ teach/forms.py
@@ -19,7 +19,7 @@ class Lesson_Form(forms.ModelForm):
     def as_p(self):
         "Returns this form rendered as HTML <p>s."
         return self._html_output(
-            normal_row = u'<p%(html_class_attr)s>%(label)s</p> %(field)s%(help_text)s',
+            normal_row = u'<p>Upload Image</p> %(field)s%(help_text)s',
             error_row = u'%s',
             row_ender = '</p>',
             help_text_html = u' <span class="helptext">%s</span>',
@@ -27,4 +27,3 @@ class Lesson_Form(forms.ModelForm):
     class Meta:
         model = Lesson
         fields = ["img"]
-        #exclude = ('teacher', 'location')
