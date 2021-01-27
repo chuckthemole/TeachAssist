5fef77451794a133408ccd888b08da866c23721c
Charles Thomas <chuckthemole@gmail.com>
2021-01-26 20:18:22 -0800

    Implement views for user profile pic

--- teach/forms.py
+++ teach/forms.py
@@ -16,5 +16,18 @@ class Lesson_Form(forms.ModelForm):
         model = Lesson
         fields = ["img"]
 
+class Teacher_Form(forms.ModelForm):
+    def as_p(self):
+        "Returns this form rendered as HTML <p>s."
+        return self._html_output(
+            normal_row = u'<p>Upload Image</p> %(field)s%(help_text)s',
+            error_row = u'%s',
+            row_ender = '</p>',
+            help_text_html = u' <span class="helptext">%s</span>',
+            errors_on_separate_row = True)
+    class Meta:
+        model = teacher
+        fields = ["img"]
+
 class Public_Form(forms.Form):
     Public=forms.CharField(label='public', widget=forms.RadioSelect(choices=CHOICES))
