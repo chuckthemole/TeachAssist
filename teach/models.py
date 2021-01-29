8ce984167d5b783f7a761da6db7cbc832b449b8b
chuckthemole <chuckthemole@gmail.com>
2021-01-29 13:13:07 -0800

    Change models to delete user img when user is deleted

--- teach/models.py
+++ teach/models.py
@@ -107,3 +107,33 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
     if not old_file == new_file:
         if os.path.isfile(old_file.path):
             os.remove(old_file.path)
+
+@receiver(models.signals.post_delete, sender=teacher)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.img:
+        if os.path.isfile(instance.img.path):
+            os.remove(instance.img.path)
+
+@receiver(models.signals.pre_save, sender=teacher)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = teacher.objects.get(pk=instance.pk).img
+    except Lesson.DoesNotExist:
+        return False
+
+    new_file = instance.img
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
