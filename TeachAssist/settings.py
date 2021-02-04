0be3050c2ec27853cebf47340dcf62b88586d52f
chuckthemole <chuckthemole@gmail.com>
2021-02-04 14:58:23 -0800

    Add secret key

--- TeachAssist/settings.py
+++ TeachAssist/settings.py
@@ -58,6 +58,9 @@ STATICFILES_DIRS = (
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
