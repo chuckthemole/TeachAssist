01ce19bc589b519db4b219231af58bc90b35dbea
chuckthemole <chuckthemole@gmail.com>
2021-02-12 13:32:43 -0800

    Change staticfiles to static

--- staticfiles/teach/js/constants.js
+++
@@ -1,7 +0,0 @@
-// Used in base.html for top left subject icon
-
-const MATH_ICON = 'https://img.icons8.com/dusk/64/000000/math.png'
-const SCIENCE_ICON = 'https://img.icons8.com/dusk/64/000000/bunsen-burner.png'
-const ENGLISH_ICON = 'https://img.icons8.com/dusk/64/000000/class.png'
-const HISTORY_ICON = 'https://img.icons8.com/dusk/64/000000/archeology.png'
-const SUBJECT_ICON = 'https://img.icons8.com/dusk/50/000000/book-and-pencil.png'
