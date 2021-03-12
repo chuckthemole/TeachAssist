0a4917d11b61eae81c5cd422fdc7e595e404a700
Charles Thomas <chuckthemole@gmail.com>
2021-03-12 13:04:24 -0800

    Add models to admin interface

--- teach/admin.py
+++ teach/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import teacher, Lesson, Review, Comment
+from .models import teacher, Lesson, Review, Comment, Quiz, Problem, Student, Submitted_Problem
 
 admin.site.register(teacher)
 admin.site.register(Lesson)
 admin.site.register(Review)
 admin.site.register(Comment)
+admin.site.register(Quiz)
+admin.site.register(Problem)
+admin.site.register(Student)
+admin.site.register(Submitted_Problem)
